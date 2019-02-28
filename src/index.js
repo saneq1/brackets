3ff1391339571f23cd8aa3e3081dfe02cb145b5d
Alexandr Kastukov <thesaneq@gmail.com>
2019-03-01 00:44:20 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = [];
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  for(var i = 0; i < str.length; i++ ) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+
+      if(str[i] == bracketsConfig[j][1] && arr[arr.length - 1] == bracketsConfig[j][0]) {
+        arr.pop();
+      } else if (str[i] == bracketsConfig[j][0]) {
+        arr.push(bracketsConfig[j][0]);
+      }
+
+    }
+  }
+
+ return arr.length == 0 ? true : false;
+
+  
+ 
 }
